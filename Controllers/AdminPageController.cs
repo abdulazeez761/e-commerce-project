8a309ad320cf97c5113bf20be8e4d67aa7d3d499
HELLENALJEZZAWI <101363253+HELLENALJEZZAWI@users.noreply.github.com>
2024-07-28 12:07:40 +0300

    initial adminlayout

---
+++ Controllers/AdminPageController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceWebsite.Controllers
+{
+    public class AdminPageController : Controller
+    {
+        public IActionResult AdminPage()
+        {
+            return View();
+        }
+        public IActionResult AdminUsersPage()
+        {
+            return View();
+        }
+        public IActionResult AdminTestomonilePage()
+        {
+            return View();
+        }
+        public IActionResult AdminOrdersPage()
+        {
+            return View();
+        }
+        public IActionResult AdminProductsAddPage()
+        {
+            return View();
+        }
+    }
+}
